ac96af135b7f3c42aa6f1ae9a4154a52e587321f
icyclestick <icebebe111@gmail.com>
2025-05-13 00:03:44 +0800

    feat(auth): implement user authentication and team management (create/join)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default async function Home() {
   }
 
   return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-muted">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <Image
               className="dark:invert"
@@ -46,7 +46,7 @@ export default async function Home() {
               <UserGreetText />
             </li>
             <li className="tracking-[-.01em]">
-              Go to register route to create/join team
+              auto redirect to route to create/join team
             </li>
           </ol>
 
